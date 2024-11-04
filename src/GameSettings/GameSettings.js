4ffe3b082d58e156cc8494232916a645efa9284c
walaszczyk-k <mrsw44@example.com>
2024-11-04 02:08:36 +0100

    GameSettings - possibleCheckNumber

--- src/GameSettings/GameSettings.js
+++ src/GameSettings/GameSettings.js
@@ -1,18 +1,30 @@
-import React, {createContext, useState} from "react";
+import React, { createContext, useState } from "react";
 
 export const GameSettingsContext = createContext({
-    difficulty: "medium",
-    username: "username",
-    setDifficulty: () => {},
-    setUsername: () => {},
+  difficulty: "medium",
+  username: "username",
+  possibleCheckNumber: 3,
+  setDifficulty: () => {},
+  setUsername: () => {},
+  setPossibleCheckNumber: () => {},
 });
 
 export const GameSettingsProvider = ({ children }) => {
-    const [difficulty, setDifficulty] = useState("medium");
-    const [username, setUsername] = useState("username");
-    return (
-        <GameSettingsContext.Provider value={{ difficulty, setDifficulty, username, setUsername }}>
-            {children}
-        </GameSettingsContext.Provider>
-    );
+  const [difficulty, setDifficulty] = useState("medium");
+  const [username, setUsername] = useState("username");
+  const [possibleCheckNumber, setPossibleCheckNumber] = useState(3);
+  return (
+    <GameSettingsContext.Provider
+      value={{
+        difficulty,
+        setDifficulty,
+        username,
+        setUsername,
+        possibleCheckNumber,
+        setPossibleCheckNumber,
+      }}
+    >
+      {children}
+    </GameSettingsContext.Provider>
+  );
 };
