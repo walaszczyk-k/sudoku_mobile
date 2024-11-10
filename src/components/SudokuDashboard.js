8e0b45b47a47a84ed4d50399965f264bf9cde185
walaszczyk-k <mrsw44@example.com>
2024-11-11 00:23:12 +0100

    SudokuDashboard - hotfix

--- src/components/SudokuDashboard.js
+++ src/components/SudokuDashboard.js
@@ -2,46 +2,46 @@ const SudokuDashboard = (props) => {
   return (
     <>
       {props.sudokuData.map((number, index) => {
-        if (number !== ".") {
-          <div className="sudoku__box__cell" key={index}>
-            {number}
-          </div>;
-        } else if (number === "." && props.running) {
-          return (
-            <input
+        if (number === ".") {
+          if (props.running) {
+            return (
+              <input
               key={index}
-              className="sudoku__box__cell"
-              type="number"
-              maxLength={1}
-              style={{
-                background: "transparent",
-                fontWeight: 900,
-                textAlign: "center",
-              }}
-              onChange={(e) => props.handleChange(index, e)}
-            ></input>
-          );
-        } else if (number === "." && !props.running) {
-          let backgroundIdx = props.isChecked
-            ? props.sudokuChecker[index]
-              ? "transparent"
-              : "red"
-            : "transparent";
-          return (
-            <input
+                className="sudoku__box__cell"
+                type="number"
+                maxLength={1}
+                style={{
+                  background: "transparent",
+                  fontWeight: 900,
+                  textAlign: "center",
+                }}
+                onChange={(e) => props.handleChange(index, e)}
+              ></input>
+            );
+          } else {
+            let backgroundIdx = props.isChecked
+              ? props.sudokuChecker[index]
+                ? "transparent"
+                : "red"
+              : "transparent";
+            return (
+              <input
               key={index}
-              className="sudoku__box__cell"
-              type="number"
-              maxLength={1}
-              style={{
-                background: backgroundIdx,
-                color: "black",
-                fontWeight: 900,
-                textAlign: "center",
-              }}
-              disabled
-            ></input>
-          );
+                className="sudoku__box__cell"
+                type="number"
+                maxLength={1}
+                style={{
+                  background: backgroundIdx,
+                  color: "black",
+                  fontWeight: 900,
+                  textAlign: "center",
+                }}
+                disabled
+              ></input>
+            );
+          }
+        } else {
+          return <div className="sudoku__box__cell">{number}</div>;
         }
       })}
     </>
